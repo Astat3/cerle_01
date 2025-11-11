3679a4769333bf2ffc8149cd379c338e9e6946e2
Adam Gallot <agallot@f6r13s11.clusters.42paris.fr>
2025-11-11 14:14:14 +0100

    gnl_vf

--- og_gnl/get_next_line.h
+++ og_gnl/get_next_line.h
@@ -6,36 +6,37 @@
 /*   By: agallot <agallot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/10 16:03:41 by agallot           #+#    #+#             */
-/*   Updated: 2025/11/10 21:27:58 by agallot          ###   ########.fr       */
+/*   Updated: 2025/11/11 14:13:42 by agallot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_H
-# define GET_H
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
 
-#ifndef BUFFER_SIZE
-#define BUFFER_SIZE 10
-#endif
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 100000
+# endif
+
 typedef struct s_list
 {
-	char         *str_buf;
-	struct s_list *next;
-}t_list;
+	char			*str_buf;
+	struct s_list	*next;
+}					t_list;
 
-void    clean(t_list **list, t_list *new_node, char *buffer);
-void    replace_node(t_list **list);
-void    copy_str(t_list *list, char *dest);
-int to_newline(t_list *list);
+void				clean(t_list **list, t_list *new_node, char *buffer);
+void				replace_node(t_list **list);
+void				copy_str(t_list *list, char *dest);
+int					to_newline(t_list *list);
 
-char *get_line(t_list *list);
-int is_new_line(t_list *list);
-t_list *get_last(t_list *list);
-void appen_list(t_list **list, char *buf);
-void    new_list(t_list **list, int fd);
-char *get_next_line(int fd);
+char				*get_line(t_list *list);
+int					is_new_line(t_list *list);
+t_list				*get_last(t_list *list);
+void				appen_list(t_list **list, char *buf);
+void				new_list(t_list **list, int fd);
+char				*get_next_line(int fd);
 
 #endif
--- og_gnl/get_next_line_utils.c
+++ og_gnl/get_next_line_utils.c
@@ -6,8 +6,126 @@
 /*   By: agallot <agallot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/10 16:04:55 by agallot           #+#    #+#             */
-/*   Updated: 2025/11/10 16:07:52 by agallot          ###   ########.fr       */
+/*   Updated: 2025/11/11 14:12:21 by agallot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+void	clean(t_list **list, t_list *new_node, char *buffer)
+{
+	t_list	*tmp;
+
+	while (*list)
+	{
+		tmp = (*list)->next;
+		free((*list)->str_buf);
+		free(*list);
+		*list = tmp;
+	}
+	*list = NULL;
+	if (new_node->str_buf[0])
+	{
+		(*list) = new_node;
+	}
+	else
+	{
+		free(buffer);
+		free(new_node);
+	}
+}
+
+void	replace_node(t_list **list)
+{
+	t_list	*last_node;
+	t_list	*new_node;
+	char	*buffer;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	new_node = malloc(sizeof(t_list));
+	if (!buffer || !new_node || !*list)
+		return ;
+	last_node = get_last(*list);
+	while (last_node->str_buf[i] != '\n' && last_node->str_buf[i] != '\0')
+		if (last_node->str_buf[i] == '\n')
+			i++;
+	while (last_node->str_buf[i])
+		buffer[j++] = last_node->str_buf[i++];
+	buffer[j] = '\0';
+	new_node->str_buf = buffer;
+	new_node->next = NULL;
+	clean(list, new_node, buffer);
+}
+
+void	copy_str(t_list *list, char *dest)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	if (!list)
+		return ;
+	while (list)
+	{
+		i = 0;
+		while (list->str_buf[i])
+		{
+			if (list->str_buf[i] == '\n')
+			{
+				dest[j++] = '\n';
+				dest[j] = '\0';
+				return ;
+			}
+			dest[j++] = list->str_buf[i++];
+		}
+		list = list->next;
+	}
+	dest[j] = '\0';
+}
+
+int	to_newline(t_list *list)
+{
+	int	i;
+	int	len;
+
+	i = 0;
+	len = 0;
+	if (list == NULL)
+		return (0);
+	while (list)
+	{
+		i = 0;
+		while (list->str_buf[i])
+		{
+			if (list->str_buf[i] == '\n')
+			{
+				len++;
+				return (len);
+			}
+			i++;
+			len++;
+		}
+		list = list->next;
+	}
+	return (len);
+}
+
+char	*get_line(t_list *list)
+{
+	char	*line;
+	int		size;
+
+	size = to_newline(list);
+	if (list == NULL)
+		return (NULL);
+	line = malloc(sizeof(char) * (size + 1));
+	if (!line)
+		return (NULL);
+	copy_str(list, line);
+	return (line);
+}
